ccba785a2a5fda192e97949ec6a769fc20759f57
adamwoolhether <adamwoolhether@gmail.com>
2023-03-18 13:11:13 +0800

    dealing with stdin

--- authentication/src/lib.rs
+++ authentication/src/lib.rs
@@ -6,9 +6,23 @@ mod tests {
     fn test_greet_user() {
         assert_eq!("Hello Adam", greet_user("Adam"));
     }
+
+    #[test]
+    fn test_case_and_trim() {
+        assert!(is_login_allowed("AdAM"));
+        assert!(is_login_allowed("    AdAM\r\n  "));
+    }
+
+    #[test]
+    fn test_login_fail() {
+        assert!(!is_login_allowed("Alice"));
+    }
 }
 
 pub fn greet_user(name: &str) ->String {
     format!("Hello {name}")
 }
 
+pub fn is_login_allowed(name: &str) -> bool {
+    name.to_lowercase().trim() == "adam"
+}
--- login/src/main.rs
+++ login/src/main.rs
@@ -1,5 +1,14 @@
-use authentication::greet_user;
+use authentication::is_login_allowed;
 
 fn main() {
-    println!("{}", greet_user("Adam"));
+    println!("Welcome to the Insecure Secure Server");
+    println!("Enter your username:");
+    let mut input = String::new();
+    let stdin = std::io::stdin();
+    stdin.read_line(&mut input).unwrap();
+    if is_login_allowed(&input) {
+        println!("Welcome, {input}");
+    } else {
+        println!("Sorry, {} is not allowed", input.trim());
+    }
 }
