1b602e96a94f011be1a530d76716f83d9e3f4fd9
adamwoolhether <adamwoolhether@gmail.com>
2023-03-18 14:22:16 +0800

    DAY1HOUR2: Enums Containing Functions

--- login/src/main.rs
+++ login/src/main.rs
@@ -1,4 +1,8 @@
-use authentication::{login, DeniedReason, LoginAction, Role};
+use authentication::*;
+
+fn user_accepted(role: &Role) {
+    println!("You are logged in as a {role:?}."); // `:?` means to "print to debug expression
+}
 
 fn main() {
     println!("Welcome to the Insecure Secure Server");
@@ -12,21 +16,9 @@ fn main() {
             println!("{} is not a known user.", input.trim());
             println!("This is where we handle new users.");
         }
-        Some(LoginAction::Accept(role)) => {
-            println!("Welcome: {}", input.trim());
-            match role {
-                Role::Admin => println!("With great power, comes great responsibility."),
-                Role::Limited => println!("You have read-only access."),
-                Role::User => println!("You have regular user access."),
-            }
-        }
-        Some(LoginAction::Denied(DeniedReason::PasswordExpired)) => {
-            println!("Unfortunately, your password has expired.");
-            println!("It needs to be 800 chars long and contain an entire sonnet.");
-        }
-        Some(LoginAction::Denied(DeniedReason::AccountLocked { reason })) => {
-            println!("Sorry, {}, your login was denied.", input.trim());
-            println!("Reason: {reason}")
-        }
+        Some(login_action) => login_action.do_login(user_accepted, |reason| {
+            println!("Access denied!");
+            println!("{reason:?}");
+        }),
     }
 }
