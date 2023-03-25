7d6fe5b68cc56087acb9280393290b2209cea8de
adamwoolhether <adamwoolhether@gmail.com>
2023-03-26 00:38:22 +0800

    DAY3HOUR1 TCP Echo Server

--- hello_tokio/src/main.rs
+++ hello_tokio/src/main.rs
@@ -1,15 +1,57 @@
-use std::time::Duration;
-use tokio::{join, spawn, task::spawn_blocking};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+    spawn,
+};
+// use tokio::{join, spawn, task::spawn_blocking};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    hello(1).await; // Running a future, we need `await`.
-                    // The `join!` marco allows us to spawn multiple futures at once and wait for them.
-    join!(hello(2), hello(3), hello(4));
+    // Basic TCP Echo server:
+    let listener = TcpListener::bind("127.0.0.1:8123").await?; // `?` to bail out with wrapped error if encountered.
+
+    loop {
+        // Wait for con & receive the address and a `TcpStream` when one arrives.
+        // Note that we don't want to wait for this connection to finish to handle the next.
+        let (mut socket, address) = listener.accept().await?;
+        // Spawn a new green-thread to handle communication with this connection,
+        // we do this with a closure within async.
+        spawn(async move {
+            // A buffer is used to read data and loop again to ready from keyboard.
+            let mut buf = vec![0; 1024];
+            loop {
+                let n = socket // Move socket from parent task into new task, read and wait for it to received data.
+                    .read(&mut buf)
+                    .await
+                    .expect("failed to read data from socket");
+
+                // Exit if there's not data.
+                if n == 0 {
+                    return;
+                }
+                socket
+                    .write_all(&buf[0..n])
+                    .await
+                    .expect("failed to write data to socket");
+            }
+        });
+    }
+
     Ok(())
 }
 
-async fn hello(n: u32) {
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+// Demonstrating basic async usage
+
+// #[tokio::main]
+// async fn main() -> anyhow::Result<()> {
+//     hello(1).await; // Running a future, we need `await`.
+//     The `join!` marco allows us to spawn multiple futures at once and wait for them.
+// join!(hello(2), hello(3), hello(4));
+// Ok(())
+// }
+
+/*async fn hello(n: u32) {
     println!("Hello {n}");
 
     if n < 10 {
@@ -42,7 +84,7 @@ async fn hello_child(n: u32) {
     // threads can continue running, and when the blocking task returns,
     // then the green-thread returns.
     let _ = spawn_blocking(|| std::thread::sleep(Duration::from_secs(1))).await;
-}
+}*/
 
 // Some other general notes for Tokio:
 /*
